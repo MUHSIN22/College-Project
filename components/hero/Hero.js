cc44162c70e2a1583d6901558c546a419deaa3b0
manshad <manshadchangampalli010@gmail.com>
2022-04-01 09:22:34 +0530

    #added:t he hero added

---
+++ components/hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {heroSection} from './Hero.module.css'
+
+const Hero = () => {
+  return (
+    <div className={heroSection}>
+
+    </div>
+  )
+}
+
+export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import Footer from '../components/footer/Footer'
 import Category from '../components/category/Category'
 import BestPortfolio from '../components/bestportfolio/BestPortfolio'
 import styles from '../styles/Home.module.css'
+import Hero from '../'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Hero/>
       <Category/>
       <BestPortfolio/>
      <Footer/>
