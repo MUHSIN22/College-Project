6fcd847674e154e5506b672700f70b3c3d2fa747
manshad <manshadchangampalli010@gmail.com>
2022-04-01 09:12:54 +0530

    #added: the best portfolio section is added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import Footer from '../components/footer/Footer'
 import Category from '../components/category/Category'
+import BestPortfolio from '../components/bestportfolio/BestPortfolio'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Category/>
+      <BestPortfolio/>
      <Footer/>
     </div>
   )
